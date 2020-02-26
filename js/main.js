e6e7e74a9110f87d14490fa0df240f991414b0bc
Adrian Rios <adrian275@gmail.com>
2020-02-26 08:37:21 -0800

    added ajax call in script of aboutus

--- js/main.js
+++ js/main.js
@@ -34,6 +34,43 @@
         animalContainer.insertAdjacentHTML("beforeend", htmlString)
     };
 
+    var zContainer = document.getElementById("z-info");
+    var btn = document.getElementById("btn");
+    btn.addEventListener("click", function(){
+        var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'https://api.songkick.com/api/3.0/events/38768509-coachella-2020.json?apikey=BcSVaxyGOG1IVR63');
+    ourRequest.onload = function() {
+        var ourData = JSON.parse(ourRequest.responseText);
+        console.log(ourData);
+    };  
+    ourRequest.send();
+    });
+    
+    function renderHTML(data) {
+        var htmlString = "";
+        for (i = 0; i < data.length; i++) {
+            htmlString += '<div>'
+            htmlString += '<img class="card-img-top" src="" alt="">'
+            htmlString += '<div class="card-body">'
+            htmlString += '<h5 class="card-title display-4">' + data[i].name + '</h5>'
+            htmlString += '<p class="card-text">Description Here</p></div>'
+            htmlString += '<ul class="list-group list-group-flush">'
+            htmlString += '<li class="list-group-item">' + 'Location:' + data[i].location + '</li>'
+            htmlString += '<li class="list-group-item">' + 'Attendees:' + data[i].attendees + '</li>'
+            htmlString += '<li class="list-group-item">' + 'Next Event:' + data[i].event + '</li>'
+            htmlString += '</ul>'
+            htmlString += '<div class="card-body">'
+            htmlString += '<a href="#" class="card-link">Card link</a>'
+            htmlString += '<a href="#" class="card-link">Another link</a>'
+            htmlString += '</div>'
+            htmlString += '</div>'
+
+        }
+        animalContainer.insertAdjacentHTML("beforeend", htmlString)
+    };
+
+    
+
 
             
             
